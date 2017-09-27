ec9b973360a71b6d29861f123a18e9aca6fa390c
triomedia <31011004+triomedia@users.noreply.github.com>
2017-09-26 18:26:16 -0600

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected on',socket.handshake.headers['user-agent']);
 
+  socket.emit('newMessage',{
+    from: 'Admin',
+    text: 'Welcome to the chat app!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'A new user has joined the chat',
+    createdAt: new Date().getTime()
+  });
   
   socket.on('createMessage', (message) => {
     console.log('create message', message);
@@ -23,6 +34,7 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    
   });
 
   socket.on('disconnect', () => {
